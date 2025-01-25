177d50bc86ff8af1116d002e1a1070a6453bf69a
Victor9955 <112949735+Victor9955@users.noreply.github.com>
2025-01-25 22:37:52 +0100

    Finished Net and wave

--- Assets/Victor/Scripts/NetManager.cs
+++ Assets/Victor/Scripts/NetManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -5,6 +6,14 @@ public class NetManager : MonoBehaviour
 {
     [SerializeField] List<NetCorner> corners;
     [SerializeField] Animator animator;
+    [SerializeField] Vector3 from;
+    [SerializeField] Vector3 to;
+    [SerializeField] float timeToGo = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(Animation());
+    }
 
     public void Damage(NetCorner corner)
     {
@@ -22,5 +31,26 @@ public class NetManager : MonoBehaviour
         {
             PlayerManager.instance.GetDamage();
         }
+    }
+
+    IEnumerator Animation()
+    {
+        float t = 0f;
+        Vector3 m_from = transform.position - from;
+        Vector3 m_to = transform.position + to;
+        while (t < timeToGo)
+        {
+            yield return null;
+            t += Time.deltaTime;
+            transform.position = Vector3.Lerp(m_from, m_to, t / timeToGo);
+        }
+        transform.position = Vector3.Lerp(m_from, m_to, 1f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position - from, 0.1f);
+        Gizmos.DrawSphere(transform.position + to, 0.1f);
     }
 }
